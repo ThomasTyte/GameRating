25cd31d1044e9327c9251f9dec7c49e82b918492
thomas <tom_tyte@hotmail.com>
2024-12-05 21:55:34 -0500

    updated navbar

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import './index.css';
 import Navbar from './component/navbar/Navbar';
 import Contact from './component/contactpage/Contact';
 import Home from './component/Homepage/Home';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-    const [currentPage, setCurrentPage] = useState('home');
-
-   
     return (
-         <BrowserRouter>
-         <Navbar />
-         <Routes>
-             <Route path="/" element={<Home />} />
-             <Route path="/home" element={<Home />} />
-             <Route path="/contact" element={<Contact />} />
-         </Routes>
-       </BrowserRouter>
-
+        <HashRouter> {/* Use HashRouter here */}
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </HashRouter>
     );
 }
 
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './navbar.css';
 
-function Navbar({ setCurrentPage }) {
+function Navbar() {
     return (
         <nav className="navbar">
             <ul className="navbar-list">
-                <li className="navbar-item" >
-                    <a href="/home">Home</a>
+                <li className="navbar-item">
+                    <Link to="/home">Home</Link>
                 </li>
-                <li className="navbar-item" >
-                    <a href="/contact">Contact</a>
+                <li className="navbar-item">
+                    <Link to="/contact">Contact</Link>
                 </li>
             </ul>
         </nav>
